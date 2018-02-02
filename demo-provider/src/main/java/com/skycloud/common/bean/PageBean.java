62b9433fae2dad7c46626ee36c32b60ce9e4483b
yc120693486 <yuanchao@chinaskycloud.com>
2018-02-02 11:02:15 +0800

    修改 测试

--- demo-provider/src/main/java/com/skycloud/common/bean/PageBean.java
+++ demo-provider/src/main/java/com/skycloud/common/bean/PageBean.java
@@ -1,9 +1,9 @@
 package com.skycloud.common.bean;
 
+import com.github.pagehelper.Page;
+
 import java.io.Serializable;
 import java.util.List;
-
-import com.github.pagehelper.Page;
 
 /**
  * 
@@ -26,7 +26,6 @@ public class PageBean<T> implements Serializable {
      * 包装Page对象，因为直接返回Page对象，在JSON处理以及其他情况下会被当成List来处理，
      * 而出现一些问题。
      * @param list          page结果
-     * @param navigatePages 页码数量
      */
     public PageBean(List<T> list) {
         if (list instanceof Page) {
